e95c11f1f8e073326c5a06c32ba5e88cff0c00f6
Mayank Chadha <77498850+mayankchadha16@users.noreply.github.com>
2022-02-07 12:37:35 +0530

    First check

--- WebCode/lenoxex.py
+++ WebCode/lenoxex.py
@@ -1,32 +1,42 @@
 from selenium import webdriver
 import time
+import os
+
+str1 = os.path.dirname(__file__)
+str2 = "resume.pdf"
+loc = str1 + "/" + str2
+
 
 def web():
-    web = webdriver.Chrome()
-    web.get('https://lenoxexsearch.com/submit-resume/')
+    web_driver = webdriver.Chrome()
+    web_driver.get('https://lenoxexsearch.com/submit-resume/')
 
     time.sleep(2)
 
-    YourName = "Ayesha"
-    name = web.find_element_by_xpath('//*[@id="candidate_name"]')
-    name.send_keys(YourName)
+    your_name = "Ayesha"
+    name = web_driver.find_element_by_xpath('//*[@id="candidate_name"]')
+    name.send_keys(your_name)
 
-    YourEmail = "Ayesha@gmail.com"
-    email = web.find_element_by_xpath('//*[@id="candidate_email"]')
-    email.send_keys(YourEmail)
+    your_email = "Ayesha@gmail.com"
+    email = web_driver.find_element_by_xpath('//*[@id="candidate_email"]')
+    email.send_keys(your_email)
 
-    YourCity = "USA"
-    city = web.find_element_by_xpath('//*[@id="candidate_location"]')
-    city.send_keys(YourEmail)
+    your_city = "USA"
+    city = web_driver.find_element_by_xpath('//*[@id="candidate_location"]')
+    city.send_keys(your_city)
 
-#Will test these later
-# sample_file = open("sample.txt", "rb")
-# upload_file = {"Uploaded file": sample_file}
-# r = requests.post(url, files = upload_file)
+    your_num = "+1 91888234243"
+    num = web_driver.find_element_by_xpath('// *[ @ id = "candidate_phone"]')
+    num.send_keys(your_num)
 
-    element = web.find_element_by_xpath('//*[@id="resume_file"]')
-# maybe like this: //input[@type="file"]
-    element.send_keys("your file path")
+    # Will test these later
+    # sample_file = open("sample.txt", "rb")
+    # upload_file = {"Uploaded file": sample_file}
+    # r = requests.post(url, files = upload_file)
 
-# Submit = web.find_element_by_xpath('//*[@id="submit-resume-form"]/p/input[5]')
-# Submit.click()
+    element = web_driver.find_element_by_xpath('//*[@id="resume_file"]')
+    # maybe like this: //input[@type="file"]
+    element.send_keys(loc)
+
+    submit = web_driver.find_element_by_xpath('//*[@id="submit-resume-form"]/p/input[5]')
+    submit.click()
